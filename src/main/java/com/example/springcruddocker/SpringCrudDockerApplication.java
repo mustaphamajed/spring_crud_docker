02c374ca301488b365b9a058030a0aac624e3c98
mustaphamajed <mustapha.majed@esprit.tn>
2024-03-25 04:35:54 +0100

    Initial commit

---
+++ src/main/java/com/example/springcruddocker/SpringCrudDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcruddocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springcruddocker/SpringCrudDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcruddocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
